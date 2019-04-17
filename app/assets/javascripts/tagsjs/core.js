b36a4d509769d5aa78eeef903f36f2c86a07e87d
Federico Ramirez <fedra.arg@gmail.com>
2019-04-17 01:02:45 -0300

    Bump version

--- app/assets/javascripts/tagsjs/core.js
+++ app/assets/javascripts/tagsjs/core.js
@@ -14,16 +14,14 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   }
 })(function () {
   var jstags = {
-    _createElement: function _createElement(tag, attributes, content) {
-      if (typeof attributes === 'string') {
-        content = attributes;
-      }
-
+    _createElement: function _createElement(tag, attributes, content, events) {
       var element = document.createElement(tag);
+
+      this._setElementAttributes(attributes, element);
 
       this._setElementContents(content, element);
 
-      this._setElementAttributes(attributes, element);
+      this._setElementEvents(events, element);
 
       return element;
     },
@@ -64,12 +62,24 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
           element.appendChild(content);
         }
       }
+    },
+    _setElementEvents: function _setElementEvents(events, element) {
+      if (!events) return;
+      Object.keys(events).forEach(function (name) {
+        element.addEventListener(name, events[name]);
+      });
     }
   };
   var AVAILABLE_TAGS = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr'];
   AVAILABLE_TAGS.forEach(function (tag) {
-    jstags[tag] = function (attributes, content) {
-      return this._createElement(tag, attributes, content);
+    jstags[tag] = function (attributes, content, events) {
+      if (typeof content === 'undefined') {
+        // when calling with a single argument
+        content = attributes;
+        attributes = undefined;
+      }
+
+      return this._createElement(tag, attributes, content, events);
     };
   });
   return jstags;
